79c617627be6dea9853192a899cbaa1338bf1de2
Norman <nhb_jr@yahoo.com>
2023-04-06 08:06:25 +0800

    Add text using main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-const myColor = document.getElementById("demo");
-myColor.style.fontSize = "200%";
+let myDemo = document.getElementById("demo").innerHTML ="Hi there!";
+myDemo = document.getElementById("demo").innerHTML = "How are you?";
+document.getElementById("demo").style.color = "green";
