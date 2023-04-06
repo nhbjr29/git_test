8baf0d36cd359e846aa90ca99803005e0e1117ce
Norman <nhb_jr@yahoo.com>
2023-04-06 08:33:56 +0800

    Modify value let const

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ let myDemo = document.getElementById("demo").innerHTML ="Hello, Javascript";// p
 //modify demo value
 myDemo = document.getElementById("demo").innerHTML = "Javascript is Fun!";//print new myDemo Value Javascript is Fun!
 document.getElementById("demo").style.color = "green";
+
+const myDemo1 = document.getElementById("demo1").innerHTML ="Hello, John";//print Hello, John
+
+myDemo1 = document.getElementById("demo1").innerHTML ="Hello, Cris";// print the new value but throws error
